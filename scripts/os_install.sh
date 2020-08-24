c2e3dec58dd931ebf073c8122c0d9036e12cf0b4
chendelin1982 <chendelin1982@126.com>
2020-08-24 10:46:02 +0800

    Update os_install.sh

--- scripts/os_install.sh
+++ scripts/os_install.sh
@@ -6,3 +6,18 @@
 2. 安装完成提示制作镜像
 3. 上传镜像到镜像制作机
 4. 镜像制作机通过程序提交到云平台审核
+
+# 阿里云
+# 安装：安骑士，云助手，cloud-init
+
+if ! python3 -c 'import setuptools' >& /dev/null; then
+  yum -y install python3-pip
+fi
+# 备份旧版cloud-init
+test -d /etc/cloud && mv /etc/cloud /etc/cloud-old
+# 下载并解压阿里云版cloud-init
+wget https://ecs-image-utils.oss-cn-hangzhou.aliyuncs.com/cloudinit/cloud-init-19.1.2.tgz
+tar -zxvf ./cloud-init-19.1.2.tgz
+# 安装cloud-init
+issue_major=$( cat /etc/redhat-release | grep -Eo '[0-9]+\.?[0-9]+' | head -1 | awk -F'.' '{printf $1}')
+bash ./cloud-init-*/tools/deploy.sh centos "$issue_major"
